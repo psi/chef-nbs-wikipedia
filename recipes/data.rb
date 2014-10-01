82eaef896c44bc3c699b1577e563caf2e2958d15
JD Harrington <jd@jdharrington.net>
2014-10-01 16:24:34 -0400

    A note about why this is dumb.

--- recipes/data.rb
+++ recipes/data.rb
@@ -1,3 +1,5 @@
+# A better idea is probably to nuke this recipe and make the importer take an
+# arg that could potentially be a local file or a URL.
 wikipedia_data_file = "http://dumps.wikimedia.org/other/pagecounts-raw/2012/2012-01/pagecounts-20120101-000000.gz"
 
 execute 'retrieve-wikipedia-data-file' do
