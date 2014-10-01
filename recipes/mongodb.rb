31a9c7c5609632cfbc7c33dd18f4f2efc194169c
JD Harrington <jd@jdharrington.net>
2014-10-01 15:45:39 -0400

    Further adventures in wrapping cookbooks

--- recipes/mongodb.rb
+++ recipes/mongodb.rb
@@ -1,13 +1,16 @@
-# Use 10gen's repo so we get a modern version of MongoDB
-include_recipe 'mongodb::10gen_repo'
+# Use the vendor's repo so we get a modern version of MongoDB
+include_recipe 'mongodb::mongodb_org_repo'
+
+node.set['mongodb']['dbconfig_file'] = '/etc/mongod.conf'
+include_recipe 'mongodb'
 
 # Override the config file template and change the location due to
 # using a newer version
-node.set['mongodb']['dbconfig_file']     = '/etc/mongod.conf'
-node.set['mongodb']['template_cookbook'] = 'nbs-wikipedia'
-node.set['mongodb']['dbconfig_file_template'] = 'mongod.conf.erb'
 
-include_recipe 'mongodb'
+r = resources(template: node['mongodb']['dbconfig_file'])
+r.cookbook 'nbs-wikipedia'
+r.source 'mongod.conf.erb'
+r.notifies :restart, 'service[mongodb]'
 
 wikipedia_data_file = "http://dumps.wikimedia.org/other/pagecounts-raw/2012/2012-01/pagecounts-20120101-000000.gz"
 
