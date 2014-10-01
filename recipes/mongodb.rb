38255fb63f0dac9fa80dfd424a44a0b38f9b8e34
JD Harrington <jd@jdharrington.net>
2014-10-01 14:37:02 -0400

    Fix up some stuff for mongodb replication

--- recipes/mongodb.rb
+++ recipes/mongodb.rb
@@ -1,4 +1,12 @@
-node.set['mongodb']['cluster_name'] = 'nbs_wikipedia'
+# Use 10gen's repo so we get a modern version of MongoDB
+include_recipe 'mongodb::10gen_repo'
+
+# Override the config file template and change the location due to
+# using a newer version
+node.set['mongodb']['dbconfig_file']     = '/etc/mongod.conf'
+node.set['mongodb']['template_cookbook'] = 'nbs-wikipedia'
+node.set['mongodb']['dbconfig_file_template'] = 'mongod.conf.erb'
+
 include_recipe 'mongodb'
 
 wikipedia_data_file = "http://dumps.wikimedia.org/other/pagecounts-raw/2012/2012-01/pagecounts-20120101-000000.gz"
